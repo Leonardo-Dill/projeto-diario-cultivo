41d3b686aa8cb2516da6b934c8eed3423032fa08
Leonardo Dill <leonardo.leo7354@gmail.com>
2026-08-17 09:34:23 -0300

    Adiciona index e style.cs, iniciando o front-end

---
+++ public/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diário de Cultivo</title>
+</head>
+
+<body>
+    <header>
+        <h1>Diário de cultivo</h1>
+    </header>
+    <main>
+        <div class="card">
+            <h2>GERENCIAR STRAINS</h2>
+            <p>Aqui você pode ver, criar, editar e apagar uma strain</p>
+            <a href="strain/listar.php" class="btn">Ir para strain</a>
+        </div>
+        <div class="card">
+            <h2>GERENCIAR PLANTAS</h2>
+            <p>Aqui você pode ver, criar, editar e apagar os registros de uma planta</p>
+            <a href="planta/listar.php" class="btn">Ir para minhas plantas</a>
+        </div>
+    </main>
+</body>
+
+</html>
